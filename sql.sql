4cf1cd7f16c7964fd2c4c6a053afba86e2dc5ed2
mamania0217 <aryanmamania17@gmail.com>
2024-04-03 17:43:57 +0530

    sql

--- sql.sql
+++ sql.sql
@@ -99,5 +99,197 @@ GROUP BY city
 ORDER BY city ASC;
 
 
+------------------------------------------------------------
+
+
+
+CREATE DATABASE COLLEGE;
+create database if not exists college
+
+USE COLLEGE;
+
+create table student (
+id INT primary key ,
+name VARCHAR (50),
+AGE int not null
+);
+
+CREATE table dept(
+id int primary key,
+name varchar(50)
+)
+
+
+CREATE TABLE teacher (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    dept_id INT,
+    FOREIGN KEY (dept_id) REFERENCES dept(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+INSERT INTO dept
+VALUES
+(101, 'ENGLSIH'),
+(102 , 'HINDI')
+
+SET sql_safe_updates = 0;
+
+
+update dept
+set name = "IT"
+where name = "HINDI"
+
+SELECT * FROM dept;
+
+INSERT INTO teacher
+VALUES 
+(101 , "MONIKA", 101),
+(102 , "ROY", 102
+
+select * from teacher
+
+drop table teacher
+
+
+insert into student values (1 , "Aryan" , 22);
+insert into student values (2, "Nancy" , 20
+
+select * from student
+
+show databases;
+show tables;
+
+insert into student (id , name, age)
+values
+(3, "Aman" , 21),
+(4, "hitesh", 24)
+
+create database xyz;
+
+use xyz;
+
+create table employeeInfo(
+id int primary key,
+name varchar(40),
+salary int not null
+)
+show tables
+
+insert into employeeInfo values (1, "adam", 25000)
+insert into employeeInfo values (2, "bob", 30000)
+insert into employeeInfo values (3, "casey", 40000)
+
+select * from employeeInfo
+
+
+drop database xyz
+
+CREATE DATABASE TEST
+USE TEST
+
+CREATE TABLE CITY(
+RollNumber int primary key,
+name varchar(50) ,
+marks int,
+grade char(1),
+City char(50)
+)
+
+insert into studentData (RollNumber , stu_name, marks , GRADE , city)
+values
+(101 , "Aryan", 100, "A" , "New Delhi" ),
+(102, "Nancy", 99, "A" , "Jaipur"),
+(103, "Aman", 85, "B" , "Haryana"),
+(104, "Hitesh" , 90, "A" , "Haryana"),
+(105, "Morrish" , 70 , "C", "Up")
+
+
+SELECT city, AVG(marks)
+FROM CITY
+GROUP BY city
+ORDER BY city ASC
+
+SELECT city, count(RollNumber)
+FROM CITY
+GROUP BY city
+HAVING MAX(marks) > 90
+ORDER BY city ASC
+
+update CITY
+SET grade  =  "O"
+where grade = "A"
+
+SET SQL_SAFE_UPDATES = 0
+
+SELECT * FROM CITY
+
+update CITY
+SET grade  =  "A"
+where grade = "O"
+
+DELETE from CITY where grade = "C"
+
+ALTER TABLE CITY
+DROP COLUMN AGE;
+
+ALTER TABLE CITY
+RENAME TO studentData
+
+SELECT * FROM studentData
+
+ALTER TABLE studentData
+MODIFY COLUMN age VARCHAR(2)
+
+ALTER TABLE studentData
+CHANGE age studentAge int
+
+INSERT INTO studentData
+VALUES
+(106, "RAHUL" , 10 , "F", "BIHAR")
+
+ALTER TABLE studentData
+DROP COLUMN studentAge
+
+TRUNCATE TABLE studentData
+
+ALTER TABLE studentData
+change name stu_name varchar(60)
+
+ALTER TABLE studentdata
+ADD COLUMN GRADE INT
+
+
+DELETE FROM studentData
+where marks > 60
+
+
+Alter table studentData
+Drop column GRADE
+
+ALTER TABLE studentData
+ADD column grade varchar(3)
+
+
+
+
+
+Select stu_name
+From studentData
+where marks > (SELECT AVG(marks)
+From studentData)
+
+SELECT stu_name
+FROM studentData
+where RollNumber %2=0
+
+SELECT stu_name , RollNumber
+From studentData
+WHERE RollNumber IN(
+SELECT RollNumber 
+FROM studentData
+where RollNumber %2=0)
+
 
 */
